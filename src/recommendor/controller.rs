4f6ecef4a86496a765f78fc5969d9be78dfc9a78
edr3x <dhunganaannanta@gmail.com>
2023-05-29 16:24:51 +0545

    `feat:` collaborative recommendatin init

--- src/recommendor/controller.rs
+++ src/recommendor/controller.rs
@@ -1,5 +1,10 @@
+use std::collections::HashSet;
+
 use actix_web::{get, web, HttpResponse, Responder};
 
+use crate::recommendor::service::{
+    collaborative_filtering_recommendations, content_based_filtering_recommendations,
+};
 use crate::AppState;
 
 use super::models::{ErrorResponse, SuccessResponse};
@@ -13,7 +18,7 @@ async fn index() -> impl Responder {
     })
 }
 
-#[get("/recom/{id}")]
+#[get("/recom/history/{id}")]
 async fn get_recommendations(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
     let history_data = match user_history(&data.db_pool, path.to_string()).await {
         Ok(data) => data,
@@ -58,8 +63,32 @@ async fn get_users() -> impl Responder {
     })
 }
 
+#[get("/recom/{id}")]
+async fn get_collaborative_filtering_recommendations(path: web::Path<String>) -> impl Responder {
+    let all_user_data = match user_data().await {
+        Ok(data) => data,
+        Err(e) => {
+            println!("Error getting info: {}", e);
+            return HttpResponse::InternalServerError().json(ErrorResponse {
+                success: false,
+                error: e.to_string(),
+            });
+        }
+    };
+
+    let colab_recoms = collaborative_filtering_recommendations(&all_user_data, &path);
+
+    // let content_recoms = content_based_filtering_recommendations(&all_user_data, &path);
+
+    HttpResponse::Ok().json(SuccessResponse {
+        success: true,
+        data: colab_recoms,
+    })
+}
+
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(index)
+        .service(get_users)
         .service(get_recommendations)
-        .service(get_users);
+        .service(get_collaborative_filtering_recommendations);
 }
--- src/recommendor/service.rs
+++ src/recommendor/service.rs
@@ -1,4 +1,4 @@
-use super::models::{UserData, UserResponse, VehicleInfo};
+use super::models::{UserData, UserResponse, Vehicle, VehicleInfo};
 
 pub async fn user_history(
     conn: &sqlx::PgPool,
@@ -43,4 +43,79 @@ pub async fn user_data() -> Result<Vec<UserData>, Box<dyn std::error::Error>> {
         .data;
 
     Ok(users)
+}
+
+pub fn collaborative_filtering_recommendations<'a>(
+    users: &'a [UserData],
+    target_user_id: &'a str,
+) -> Vec<&'a Vehicle> {
+    let target_user = users.iter().find(|user| user.id == target_user_id);
+
+    if let Some(user) = target_user {
+        let booked_vehicle_ids: Vec<&str> = user
+            .booking
+            .iter()
+            .map(|booking| booking.vehicle.id.as_str())
+            .collect();
+
+        let similar_users: Vec<&UserData> = users
+            .iter()
+            .filter(|&u| u.id != target_user_id)
+            .filter(|u| {
+                u.booking
+                    .iter()
+                    .any(|booking| booked_vehicle_ids.contains(&booking.vehicle.id.as_str()))
+            })
+            .collect();
+
+        let mut recommended_vehicles: Vec<&Vehicle> = Vec::new();
+        for user in similar_users {
+            recommended_vehicles.extend(
+                user.booking
+                    .iter()
+                    .filter(|booking| !booked_vehicle_ids.contains(&booking.vehicle.id.as_str()))
+                    .map(|booking| &booking.vehicle),
+            );
+        }
+
+        recommended_vehicles
+    } else {
+        Vec::new()
+    }
+}
+
+pub fn content_based_filtering_recommendations<'a>(
+    users: &'a [UserData],
+    target_user_id: &'a str,
+) -> Vec<&'a Vehicle> {
+    let target_user = users.iter().find(|user| user.id == target_user_id);
+
+    if let Some(user) = target_user {
+        let booked_vehicle_features: Vec<&str> = user
+            .booking
+            .iter()
+            .flat_map(|booking| booking.vehicle.features.color.split(", "))
+            .collect();
+
+        let mut recommended_vehicles: Vec<&Vehicle> = Vec::new();
+        for user in users {
+            if user.id != target_user_id {
+                for booking in &user.booking {
+                    if booking
+                        .vehicle
+                        .features
+                        .color
+                        .split(", ")
+                        .any(|feature| booked_vehicle_features.contains(&feature))
+                    {
+                        recommended_vehicles.push(&booking.vehicle);
+                    }
+                }
+            }
+        }
+
+        recommended_vehicles
+    } else {
+        Vec::new()
+    }
 }
